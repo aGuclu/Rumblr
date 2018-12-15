fc0e633ffbd1c8d25da2ccd6c72c745701c80d09
Ahmet Guclu <ahmetguclu.ag@gmail.com>
2018-12-15 09:44:27 -0500

    re-linked JS + CSS

--- public/main.js
+++ public/main.js
@@ -1,31 +1,31 @@
 console.log("SUCCESS");
-//
+
 // document.getElementById('newpost').addEventListener('click', load_out_big);
 // document.getElementById('signup').addEventListener('click', load_out_small);
 // document.getElementById('login').addEventListener('click', load_out_small);
 // document.getElementById('myacct').addEventListener('click', load_out_big);
-//
-//
-// function load_out_small(url) {
-//   document.getElementById('canvas').style.transition = "0.4s";
-//   document.getElementById('canvas').style.width = "300px";
-//   document.getElementById('canvas').style.height = "300px";
-//   setTimeout( function() {
-//     window.location = url;
-//   }, 400);
-// }
-//
-// function load_out_big(url) {
-//   document.getElementById('canvas').style.transition = "0.4s";
-//   document.getElementById('canvas').style.width = "700px";
-//   document.getElementById('canvas').style.height = "700px";
-//   setTimeout( function() {
-//     window.location = url;
-//   }, 400);
-// }
-//
-// function loginTransition() {
-//   document.getElementById('canvas').style.transition = "0.4s";
-//   document.getElementById('canvas').style.width = "700px";
-//   document.getElementById('canvas').style.height = "700px";
-// }
+
+
+function load_out_small(url) {
+  document.getElementById('canvas').style.transition = "0.4s";
+  document.getElementById('canvas').style.width = "300px";
+  document.getElementById('canvas').style.height = "300px";
+  setTimeout( function() {
+    window.location = url;
+  }, 400);
+}
+
+function load_out_big(url) {
+  document.getElementById('canvas').style.transition = "0.4s";
+  document.getElementById('canvas').style.width = "700px";
+  document.getElementById('canvas').style.height = "700px";
+  setTimeout( function() {
+    window.location = url;
+  }, 400);
+}
+
+function loginTransition() {
+  document.getElementById('canvas').style.transition = "0.4s";
+  document.getElementById('canvas').style.width = "700px";
+  document.getElementById('canvas').style.height = "700px";
+}
